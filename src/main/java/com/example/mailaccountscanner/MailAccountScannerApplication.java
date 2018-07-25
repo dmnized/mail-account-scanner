fc3242be4d353a764a84783339de73205ab5a2ff
Curcio Stefano <stefano.curcio@lascaux.it>
2018-07-25 22:05:32 +0200

    First commit

---
+++ src/main/java/com/example/mailaccountscanner/MailAccountScannerApplication.java
@@ -0,0 +1,12 @@
+package com.example.mailaccountscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailAccountScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailAccountScannerApplication.class, args);
+	}
+}
